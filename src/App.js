6a99a9e7252da96f92df8dc6596acb63b6fcddff
nicolaslimaalbuquerque <Nicolas.albuquerquerp@gmail.com>
2024-08-08 09:10:55 -0300

    ok

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import Footer from "./components/Footer";
+import Banner from "./components/Banner";
+import Header from "./components/Header";
+
+
+function App() {
+  return (
+    <>
+      <Header/>
+      <Banner/>
+      <h1>ETEC MCM</h1>
+      <p>Teste de React</p>
+      <Footer/>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Footer/index.js
@@ -0,0 +1,11 @@
+import styles from './Footer.module.css';
+
+function Footer() {
+    return (
+        <footer className={styles.footer}>
+        <h2>desenvolvido por Nicolas Albuquerque &reg; 2024</h2>
+        </footer>
+    );
+}
+
+export default Footer;
