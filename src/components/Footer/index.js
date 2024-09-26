5bc1de01016341ce6064c68a4b616291dac2a071
Your Nam <Nicolas.albuquerquerp@gmail.com>
2024-09-26 08:03:14 -0300

    .

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -3,7 +3,7 @@ import styles from "./Footer.module.css";
 function Footer() {
     return (
         <footer className={styles.footer}>
-            <h2>EtecFlix &copy; Desenvolvido por Murilo Gama 2024</h2>
+            <h2>EtecFlix &copy; Desenvolvido por Nicolas Lima Albuquerque 2024</h2>
         </footer>
     );
 }
